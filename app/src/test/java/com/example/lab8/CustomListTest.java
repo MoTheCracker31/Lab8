6f2adf6edfcc99bc8618aa8225d3d0cb1da086bd
MoTheCracker31 <113314673+MoTheCracker31@users.noreply.github.com>
2024-10-23 17:59:30 -0600

    B
    should fail CI

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,12 +5,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private CustomList list;
 
+
+    /*
+    Create a mock list for my city list
+    @return list
+     */
+
+
+    public CustomList MockCityList(){
+        list  = new CustomList(null, new ArrayList<>());
+        return list;
+    }
+
+    @Test
+    public void addCityList(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan","SK"));
+        assertEquals(list.getCount(), listSize + 1);
+    }
 }
